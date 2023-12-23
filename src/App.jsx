41029b7d68542c2b4f841c6a99694b2e22724810
Kislame <shadoooo198@gmail.com>
2023-12-23 14:56:51 +0100

    updating firt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import useMediaquery from './hooks/useMediaQuery';
-import Landing from './scenes/Landing';
+// import Landing from './scenes/Landing';
 import Navbar from './scenes/Navbar';
 
-import MySkills from './scenes/MySkills';
-import { motion } from 'framer-motion';
-import Projects from './scenes/Projects';
-import Contact from './scenes/Contact';
+// import MySkills from './scenes/MySkills';
+// import { motion } from 'framer-motion';
+// import Projects from './scenes/Projects';
+// import Contact from './scenes/Contact';
+// import DotGroup from './scenes/DotGroup';
 
 function App() {
-  const cursorRef = useRef();
   const [selectedPage, setSelectedPage] = useState('home');
   const [isTopOfPage, setIsTopOfPage] = useState(true);
-  const isAboveMediumScreens = useMediaquery('(min-width: 1060px)');
+  // const isAboveMediumScreens = useMediaquery('(min-width: 1060px)');
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,49 +33,6 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-
-      <div>
-        <div className="w-5/6  mx-auto md:h-full">
-          <motion.div
-            margin="0 0 -200px 0"
-            amount="all"
-            onViewportEnter={() => setSelectedPage('home')}
-          >
-            <Landing setSelectedPage={setSelectedPage} />
-          </motion.div>
-        </div>
-        <div className="h-[178px] bg-pattern bg-no-repeat bg-cover " />
-      </div>
-
-      <div className=" md:h-full ">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage('skills')}
-        >
-          <MySkills />
-        </motion.div>
-      </div>
-
-      <div className="w-5/6 mx-auto">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage('projects')}
-        >
-          <Projects />
-        </motion.div>
-      </div>
-
-      <div className="w-5/6 mx-auto md:h-full">
-        <motion.div
-          margin="0 0 -200px 0"
-          amount="all"
-          onViewportEnter={() => setSelectedPage('contact')}
-        >
-          <Contact />
-        </motion.div>
-      </div>
     </div>
   );
 }
--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -11,3 +11,52 @@ const Footer = () => {
 };
 
 export default Footer;
+
+{
+  /* <div className="w-5/6  mx-auto md:h-full">
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+
+          <motion.div
+            margin="0 0 -200px 0"
+            amount="all"
+            onViewportEnter={() => setSelectedPage('home')}
+          >
+            <Landing setSelectedPage={setSelectedPage} />
+          </motion.div>
+        </div>
+        <div className="h-[178px] bg-pattern bg-no-repeat bg-cover " />
+      </div>
+
+      <div className=" md:h-full ">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage('skills')}
+        >
+          <MySkills />
+        </motion.div>
+      </div>
+
+      <div className="w-5/6 mx-auto">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage('projects')}
+        >
+          <Projects />
+        </motion.div>
+      </div>
+
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage('contact')}
+        >
+          <Contact />
+        </motion.div>
+      </div> */
+}
